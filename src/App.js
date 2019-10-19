42e86bbc7b26d31a275b0f1fd0cc5d5f75e95660
riverhuang-batam <riverhuang011@gmail.com>
2019-10-19 11:09:24 +0700

    e-phone webdesign

---
+++ src/App.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import samsung1 from './samsung1.jpg'
+import samsung2 from './samsung2.jpg'
+import samsung3 from './samsung3.jpg'
+import samsung4 from './samsung4.jpg'
+import samsung5 from './samsung5.jpg'
+import samsung6 from './samsung6.jpg'
+import samsung7 from './samsung7.jpg'
+import samsung8 from './samsung8.jpg'
+import samsung9 from './samsung9.jpeg'
+import samsung10 from './samsung10.jpg'
+import samsung11 from './samsung11.jpg'
+import samsung12 from './samsung12.jpg'
+import iphone1 from './iphone1.png'
+import iphone2 from './iphone2.png'
+import iphone3 from './iphone3.jpg'
+import iphone4 from './iphone4.jpg'
+import iphone5 from './iphone5.jpg'
+import iphone6 from './iphone6.jpg'
+import iphone7 from './iphone7.jpg'
+import iphone8 from './iphone8.jpg'
+import {Card, CardImg, CardText, CardTitle, CardSubtitle, Button, Container, Row, Col} from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+     <Container>
+       <h1 className="text-center mt-4">welcome to E-phone website</h1>
+      <Row>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung1}/>
+        <CardTitle>samsung1</CardTitle>
+        <CardSubtitle>samsung1</CardSubtitle>
+        <CardText>samsung1 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung2}/>
+        <CardTitle>samsung2</CardTitle>
+        <CardSubtitle>samsung2</CardSubtitle>
+        <CardText>samsung2 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung3}/>
+        <CardTitle>samsung3</CardTitle>
+        <CardSubtitle>samsung3</CardSubtitle>
+        <CardText>samsung3 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung4}/>
+        <CardTitle>samsung4</CardTitle>
+        <CardSubtitle>samsung4</CardSubtitle>
+        <CardText>samsung4 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung5}/>
+        <CardTitle>samsung5</CardTitle>
+        <CardSubtitle>samsung5</CardSubtitle>
+        <CardText>samsung5 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung6}/>
+        <CardTitle>samsung6</CardTitle>
+        <CardSubtitle>samsung6</CardSubtitle>
+        <CardText>samsung6 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung7}/>
+        <CardTitle>samsung7</CardTitle>
+        <CardSubtitle>samsung7</CardSubtitle>
+        <CardText>samsung7 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung8}/>
+        <CardTitle>samsung8</CardTitle>
+        <CardSubtitle>samsung8</CardSubtitle>
+        <CardText>samsung8 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung9}/>
+        <CardTitle>samsung9</CardTitle>
+        <CardSubtitle>samsung9</CardSubtitle>
+        <CardText>samsung9 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung10}/>
+        <CardTitle>samsung10</CardTitle>
+        <CardSubtitle>samsung10</CardSubtitle>
+        <CardText>samsung10 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung11}/>
+        <CardTitle>samsung11</CardTitle>
+        <CardSubtitle>samsung11</CardSubtitle>
+        <CardText>samsung11 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={samsung12}/>
+        <CardTitle>samsung12</CardTitle>
+        <CardSubtitle>samsung12</CardSubtitle>
+        <CardText>samsung12 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone1}/>
+        <CardTitle>iphone1</CardTitle>
+        <CardSubtitle>iphone1</CardSubtitle>
+        <CardText>iphone1 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone2}/>
+        <CardTitle>iphone2</CardTitle>
+        <CardSubtitle>iphone2</CardSubtitle>
+        <CardText>iphone2 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone3}/>
+        <CardTitle>iphone3</CardTitle>
+        <CardSubtitle>iphone3</CardSubtitle>
+        <CardText>iphone3 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone4}/>
+        <CardTitle>iphone4</CardTitle>
+        <CardSubtitle>iphone4</CardSubtitle>
+        <CardText>iphone4 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone5}/>
+        <CardTitle>iphone5</CardTitle>
+        <CardSubtitle>iphone5</CardSubtitle>
+        <CardText>iphone5 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone6}/>
+        <CardTitle>iphone6</CardTitle>
+        <CardSubtitle>iphone6</CardSubtitle>
+        <CardText>iphone6 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone7}/>
+        <CardTitle>iphone7</CardTitle>
+        <CardSubtitle>iphone7</CardSubtitle>
+        <CardText>iphone7 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+        <Col className="mt-4" md="3">
+      <Card body outline color="grey">
+        <CardImg top width="100%" src={iphone8}/>
+        <CardTitle>iphone8</CardTitle>
+        <CardSubtitle>iphone8</CardSubtitle>
+        <CardText>iphone8 phone</CardText>
+        <Button body inverse color="success">buy</Button>
+      </Card>
+        </Col>
+      </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
